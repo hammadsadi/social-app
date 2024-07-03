2f52a6e2bc7c73e35af0dfd5a807735a99cce78b
hammadsadi <hammad.sadi@yahoo.com>
2024-07-04 02:09:16 +0600

    User follow unfollow route and controllers added

--- social-api/controllers/userControllers.js
+++ social-api/controllers/userControllers.js
@@ -13,3 +13,38 @@ export const getUserProfile = async (req, res) => {
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+// User Follow unFollow
+export const userFollowUnFollow = async (req, res) => {
+  try {
+    // Get id
+    const { id } = req.params;
+    const userModify = await User.findById(id);
+    const currentUser = await User.findById(req.user._id);
+
+    // Check User
+    if (id === req.user._id.toString())
+      return res
+        .status(400)
+        .json({ error: "You Can not Follow/UnFollow Yourself" });
+    if (!userModify || !currentUser)
+      return res.status(404).json({ error: "User Not Found" });
+
+    // check Following
+    const isFollowing = currentUser.following.includes(id);
+    if (isFollowing) {
+      // UnFollow
+      await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
+      res.status(200).json({ message: "User UnFollowed Successful" });
+    } else {
+      // Follow
+      await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
+      res.status(200).json({ message: "User Followed Successful" });
+    }
+  } catch (error) {
+    console.log("error in userFollowUnFollow Controller", error.message);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
--- social-api/routes/userRoutes.js
+++ social-api/routes/userRoutes.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { protectedRoute } from "../middlewares/protectedRoute.js";
-import { getUserProfile } from "../controllers/userControllers.js";
+import {
+  getUserProfile,
+  userFollowUnFollow,
+} from "../controllers/userControllers.js";
 
 // Init Router
 const router = express.Router();
 
 // Routing
 router.get("/profile/:username", protectedRoute, getUserProfile);
+router.post("/follow/:id", protectedRoute, userFollowUnFollow);
 
 export default router;
