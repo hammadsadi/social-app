08dbed7c5853210697fb5e109325cfeab53b853a
hammadsadi <hammad.sadi@yahoo.com>
2024-07-07 16:07:09 +0600

    Post Delete Route and Controllers Added

--- social-api/controllers/createPostControllers.js
+++ social-api/controllers/createPostControllers.js
@@ -1,6 +1,6 @@
 import Post from "../models/post/post.js";
 import User from "../models/user/user.js";
-
+import { v2 as cloudinary } from "cloudinary";
 export const createPost = async (req, res) => {
   try {
     const { text } = req.body;
@@ -17,6 +17,12 @@ export const createPost = async (req, res) => {
     if (!text & !img)
       return res.status(400).json({ error: "Post Must have Text or Image" });
 
+    // Image Upload
+    if (img) {
+      const resImage = await cloudinary.uploader.upload(img);
+      img = resImage.secure_url;
+    }
+
     const newPost = new Post({
       user: userId,
       text,
@@ -27,6 +33,38 @@ export const createPost = async (req, res) => {
     res.status(200).json(newPost);
   } catch (error) {
     console.log("error in createPost Controller", error.message);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check Post
+    if (!post) {
+      return res.status(404).json({ error: "Post Not Found" });
+    }
+
+    if (post.user.toString() !== req.user._id.toString()) {
+      return res
+        .status(401)
+        .json({ error: "You are Not Authorized for Delete This Post!" });
+    }
+
+    // Delete Img
+    if (post.img) {
+      await cloudinary.uploader.destroy(
+        post.img.split("/").pop().split(".")[0]
+      );
+    }
+
+    // Delete Post
+    await Post.findByIdAndDelete(req.params.id);
+
+    return res.status(200).json({ message: "Post Deleted Successful" });
+  } catch (error) {
+    console.log("error in deletePost Controller", error.message);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
--- social-api/routes/postRoutes.js
+++ social-api/routes/postRoutes.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { protectedRoute } from "../middlewares/protectedRoute.js";
-import { createPost } from "../controllers/createPostControllers.js";
+import {
+  createPost,
+  deletePost,
+} from "../controllers/createPostControllers.js";
 
 // Init Router
 const router = express.Router();
 
 // Create Post
 router.post("/create", protectedRoute, createPost);
+router.delete("/:id", protectedRoute, deletePost);
 
 // Export Default
 export default router;
